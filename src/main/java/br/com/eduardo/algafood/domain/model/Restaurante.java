0b329e3824b7293461048e5847dec5f058566b58
xfire741 <eduardovictor28@gmail.com>
2022-11-24 16:48:33 -0300

    Join Column, nullable

--- src/main/java/br/com/eduardo/algafood/domain/model/Restaurante.java
+++ src/main/java/br/com/eduardo/algafood/domain/model/Restaurante.java
@@ -5,7 +5,11 @@ import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -17,12 +21,17 @@ public class Restaurante {
 	
 	@EqualsAndHashCode.Include
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(length = 30)
 	private String nome;
 	
-	@Column(name = "taxa_frete")
-	private BigDecimal taxaFrete;	
+	@Column(name = "taxa_frete", nullable = false)
+	private BigDecimal taxaFrete;
+	
+	@ManyToOne
+	@JoinColumn(name = "cozinha_id")
+	private Cozinha cozinha;
 	
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
-insert into cozinha values (null, 'Tailandesa');
-insert into cozinha values (null, 'Indiana');
+insert into cozinha values (1, 'Tailandesa');
+insert into cozinha values (2, 'Indiana');
+
+insert into restaurante values (null, 'Thai Gourmet', 10, 1);
+insert into restaurante values (null, 'Thai Gourmet', 9.50, 1);
+insert into restaurante values (null, 'Tuk Tuk Comida', 15, 2);
