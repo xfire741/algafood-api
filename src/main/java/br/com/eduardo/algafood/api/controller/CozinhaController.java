772b6ecd43d6207c2aba4dcb91ba18e8bf71472b
xfire741 <eduardovictor28@gmail.com>
2022-11-25 08:28:09 -0300

    Buscar cozinha

--- src/main/java/br/com/eduardo/algafood/api/controller/CozinhaController.java
+++ src/main/java/br/com/eduardo/algafood/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.eduardo.algafood.domain.model.Cozinha;
@@ -22,4 +23,9 @@ public class CozinhaController {
 		return cozinhaRepository.listar();
 	}
 		
+	@GetMapping("/{id}")
+	public Cozinha buscar(@PathVariable("id") Long id) {
+		return cozinhaRepository.buscar(id);
+	}
+	
 }
