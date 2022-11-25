46a629ba3c2a5478d5b933b28fcdb2e78fe37838
xfire741 <eduardovictor28@gmail.com>
2022-11-25 11:39:33 -0300

    Metodo Atualizar

--- src/main/java/br/com/eduardo/algafood/api/controller/CozinhaController.java
+++ src/main/java/br/com/eduardo/algafood/api/controller/CozinhaController.java
@@ -2,6 +2,7 @@ package br.com.eduardo.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,33 +24,47 @@ import br.com.eduardo.algafood.domain.repository.CozinhaRepository;
 @RestController
 @RequestMapping("/cozinhas")
 public class CozinhaController {
-	
+
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 
 	@GetMapping
-	public List<Cozinha> listar(){
+	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable("id") Long id) {
 		Cozinha cozinha = cozinhaRepository.buscar(id);
-		if(cozinha != null) {
+		if (cozinha != null) {
 			return ResponseEntity.ok(cozinha);
 		}
 		return ResponseEntity.notFound().build();
-		}
-	
+	}
+
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
 	public CozinhasXmlWrapper listarXml() {
 		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
 		return cozinhaRepository.salvar(cozinha);
 	}
-	
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long id, @RequestBody Cozinha cozinha) {
+		
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(id);
+		if (cozinhaAtual != null) {
+			
+			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+			return ResponseEntity.ok(cozinhaRepository.salvar(cozinhaAtual));
+		}
+		
+		return ResponseEntity.notFound().build();
+		
+	}
+
 }
