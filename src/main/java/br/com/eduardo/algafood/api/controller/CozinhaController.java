5a02c85f3347f9ec6c408741bea1d5ae47bef895
xfire741 <eduardovictor28@gmail.com>
2022-11-25 11:15:31 -0300

    Metodo salvar

--- src/main/java/br/com/eduardo/algafood/api/controller/CozinhaController.java
+++ src/main/java/br/com/eduardo/algafood/api/controller/CozinhaController.java
@@ -9,7 +9,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.eduardo.algafood.api.model.CozinhasXmlWrapper;
@@ -34,15 +37,18 @@ public class CozinhaController {
 		if(cozinha != null) {
 			return ResponseEntity.ok(cozinha);
 		}
-		
-		//return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		return ResponseEntity.notFound().build();
-			
 		}
 	
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
 	public CozinhasXmlWrapper listarXml() {
 		return new CozinhasXmlWrapper(cozinhaRepository.listar());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
 	}
 	
 }
