b59c64bb2e00a3a5c7c189a334bdcf775e6a12e5
xfire741 <eduardovictor28@gmail.com>
2022-11-25 09:58:29 -0300

    Anotação status http

--- src/main/java/br/com/eduardo/algafood/api/controller/CozinhaController.java
+++ src/main/java/br/com/eduardo/algafood/api/controller/CozinhaController.java
@@ -3,10 +3,12 @@ package br.com.eduardo.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.eduardo.algafood.api.model.CozinhasXmlWrapper;
@@ -24,7 +26,8 @@ public class CozinhaController {
 	public List<Cozinha> listar(){
 		return cozinhaRepository.listar();
 	}
-		
+	
+	@ResponseStatus(HttpStatus.OK)
 	@GetMapping("/{id}")
 	public Cozinha buscar(@PathVariable("id") Long id) {
 		return cozinhaRepository.buscar(id);
--- src/main/java/br/com/eduardo/algafood/api/model/CozinhasXmlWrapper.java
+++ src/main/java/br/com/eduardo/algafood/api/model/CozinhasXmlWrapper.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
 import br.com.eduardo.algafood.domain.model.Cozinha;
