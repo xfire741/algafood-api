fa5ed8078f1ae6561b16378465ace827cf142183
xfire741 <eduardovictor28@gmail.com>
2022-11-29 17:58:23 -0300

    FindByNome

--- src/main/java/br/com/eduardo/algafood/api/controller/TesteController.java
+++ src/main/java/br/com/eduardo/algafood/api/controller/TesteController.java
@@ -1,19 +1,27 @@
 package br.com.eduardo.algafood.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.eduardo.algafood.domain.model.Cozinha;
+import br.com.eduardo.algafood.domain.repository.CozinhaRepository;
 
 
 @RestController
 @RequestMapping("/teste")
 public class TesteController {
 
-//	@Autowired
-//	private CozinhaRepositoryImpl cozinhaRepository;
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
 	
-//	@GetMapping("/cozinhas/por-nome")
-//	public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
-//		return cozinhaRepository.consultarPorNome(nome);
-//	}
+	@GetMapping("/cozinhas/por-nome")
+	public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
+		return cozinhaRepository.findByNome(nome);
+	}
 	
 }
--- src/main/java/br/com/eduardo/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/br/com/eduardo/algafood/domain/repository/CozinhaRepository.java
@@ -1,5 +1,7 @@
 package br.com.eduardo.algafood.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +11,7 @@ import br.com.eduardo.algafood.domain.model.Cozinha;
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long>{
 
 	
-	//List<Cozinha> consultarPorNome(String nome);
+	List<Cozinha> findByNome(String nome);
 	
 	
 }
