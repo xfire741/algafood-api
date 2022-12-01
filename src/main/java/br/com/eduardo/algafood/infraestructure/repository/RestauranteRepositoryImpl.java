a2f2ce5dedfd7f96a25798ba3961fddd2dc69533
xfire741 <eduardovictor28@gmail.com>
2022-12-01 13:26:58 -0300

    Consulta dinâmica com Java

--- src/main/java/br/com/eduardo/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/br/com/eduardo/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
@@ -8,6 +8,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
@@ -16,19 +18,26 @@ import br.com.eduardo.algafood.domain.repository.RestauranteRepositoryQueries;
 
 @Repository
 public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
-	
+
 	@PersistenceContext
 	private EntityManager manager;
-	
+
 	@Override
-	public List<Restaurante> find(String nome, 
-			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
-		
+
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-		
-		criteria.from(Restaurante.class);
-		
+
+		Root<Restaurante> root = criteria.from(Restaurante.class);
+
+		Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
+		Predicate taxaInicialPredicate = builder
+				.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+		Predicate taxaFinalPredicate = builder
+				.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+
+		criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
+
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		return query.getResultList();
 	}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,7 +2,7 @@ insert into cozinha values (1, 'Tailandesa');
 insert into cozinha values (2, 'Indiana');
 
 insert into restaurante values (null, 'Thai Gourmet', 10, 1);
-insert into restaurante values (null, 'Thai Gourmet', 9.50, 1);
+insert into restaurante values (null, 'Thai Nama', 9.50, 1);
 insert into restaurante values (null, 'Tuk Tuk Comida', 15, 2);
 
     insert into estado (id, nome) values (1, 'Minas Gerais');
