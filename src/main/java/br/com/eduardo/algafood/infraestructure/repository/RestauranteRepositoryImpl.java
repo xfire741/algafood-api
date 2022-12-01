6c03471b69d6f8ebf1f912df70ef1ad5dcf010a1
xfire741 <eduardovictor28@gmail.com>
2022-12-01 13:35:10 -0300

    Consulta dinâmica com Java 2

--- src/main/java/br/com/eduardo/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/br/com/eduardo/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
@@ -1,6 +1,7 @@
 package br.com.eduardo.algafood.infraestructure.repository;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -12,6 +13,7 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import br.com.eduardo.algafood.domain.model.Restaurante;
 import br.com.eduardo.algafood.domain.repository.RestauranteRepositoryQueries;
@@ -29,14 +31,22 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
 
 		Root<Restaurante> root = criteria.from(Restaurante.class);
+		
+		var predicates = new ArrayList<Predicate>();
 
-		Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
-		Predicate taxaInicialPredicate = builder
-				.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
-		Predicate taxaFinalPredicate = builder
-				.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+		if (StringUtils.hasText(nome)) {
+		predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+		}
+		
+		if (taxaFreteInicial != null) {
+		predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+		}
+		
+		if (taxaFreteFinal != null) {
+		predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+		}
 
-		criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
+		criteria.where(predicates.toArray(new Predicate[0]));
 
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		return query.getResultList();
