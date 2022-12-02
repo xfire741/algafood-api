e7b128b56a79a1d65769556fac69c5baa1b50498
xfire741 <eduardovictor28@gmail.com>
2022-12-01 21:14:02 -0300

    Many-to-Many

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -21,3 +21,10 @@ insert into restaurante values (null, 'Tuk Tuk Comida', 15, 2);
 
     insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
     insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
+    
+    insert into restaurante_forma_pagamento values (1,1);
+    insert into restaurante_forma_pagamento values (1,2);
+    insert into restaurante_forma_pagamento values (1,3);
+   	insert into restaurante_forma_pagamento values (2,3);
+   	insert into restaurante_forma_pagamento values (3,2);
+   	insert into restaurante_forma_pagamento values (3,3);
