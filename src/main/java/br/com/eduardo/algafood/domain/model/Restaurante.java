698bde649bd3e383ccdf52208bb24736a2f67b0a
xfire741 <eduardovictor28@gmail.com>
2022-12-03 18:00:45 -0300

    fetch Lazy, otimizando consultas

--- src/main/java/br/com/eduardo/algafood/domain/model/Restaurante.java
+++ src/main/java/br/com/eduardo/algafood/domain/model/Restaurante.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -41,7 +42,8 @@ public class Restaurante {
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 	
-	@ManyToOne
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
 	
