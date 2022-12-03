7ee3b400b89b87456bcf0946acabe67fa5d61351
xfire741 <eduardovictor28@gmail.com>
2022-12-03 20:13:11 -0300

    Configurando hikari Connection Pool

--- src/main/java/br/com/eduardo/algafood/domain/model/Restaurante.java
+++ src/main/java/br/com/eduardo/algafood/domain/model/Restaurante.java
@@ -61,7 +61,7 @@ public class Restaurante {
 	@Column(nullable = false, columnDefinition = "datetime")
 	private LocalDateTime dataAtualizacao;
 	
-	
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "restaurante_forma_pagamento",
 		joinColumns = @JoinColumn(name = "restaurante_id"),
--- src/main/java/br/com/eduardo/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/br/com/eduardo/algafood/domain/repository/RestauranteRepository.java
@@ -16,7 +16,7 @@ public interface RestauranteRepository
 extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
 
 	
-	@Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+	@Query("from Restaurante r join fetch r.cozinha")
 	List<Restaurante> findAll();
 	
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxainicial, BigDecimal taxaFinal);
