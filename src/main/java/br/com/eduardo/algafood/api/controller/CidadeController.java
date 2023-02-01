ed0271a469f0ebabbaecafeaf51df3e0dd6fad8d
xfire741 <eduardovictor28@gmail.com>
2023-02-01 18:56:08 -0300

    ExceptionHandler global

--- src/main/java/br/com/eduardo/algafood/api/controller/CidadeController.java
+++ src/main/java/br/com/eduardo/algafood/api/controller/CidadeController.java
@@ -1,6 +1,5 @@
 package br.com.eduardo.algafood.api.controller;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
@@ -8,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.eduardo.algafood.api.exceptionhandler.Problema;
-import br.com.eduardo.algafood.domain.exception.EntidadeNaoEncontradaException;
 import br.com.eduardo.algafood.domain.exception.EstadoNaoEncontradaException;
 import br.com.eduardo.algafood.domain.exception.NegocioException;
 import br.com.eduardo.algafood.domain.model.Cidade;
@@ -81,28 +77,6 @@ public class CidadeController {
 			throw new NegocioException(e.getMessage(), e);
 
 		}
-	}
-
-	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(
-			EntidadeNaoEncontradaException e) {
-		Problema problema = Problema
-				.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
-
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
-
-	}
-	
-	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(NegocioException e) {
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
-
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
-
 	}
 
 }
