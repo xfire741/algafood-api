5b855bc1eef9fa715356f5dcb87676302c95eb1e
xfire741 <eduardovictor28@gmail.com>
2023-02-07 20:45:07 -0300

    Estendendo o Problem Details para adicionar as propriedades com constraints violadas

--- src/main/java/br/com/eduardo/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/eduardo/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.eduardo.algafood.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -236,7 +238,18 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente";
 		
-		Problem problem = createProblemBuilder(status, problemType, detail, LocalDateTime.now()).userMessage(detail).build();
+		BindingResult bindingResult = ex.getBindingResult();
+		
+		List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
+				.map(fieldError -> Problem.Field.builder()
+						.name(fieldError.getField())
+						.userMessage(fieldError.getDefaultMessage())
+						.build())
+				.collect(Collectors.toList());
+		
+		Problem problem = createProblemBuilder(status, problemType, detail, LocalDateTime.now())
+				.userMessage(detail)
+				.fields(problemFields).build();
 		
 		return handleExceptionInternal(ex, problem, headers, status, request);
 	}
