c99de862ae2539d5997296d211d6b4e9757907ef
xfire741 <eduardovictor28@gmail.com>
2023-02-08 16:11:42 -0300

    Validacao em cascada
    Adicionando NotNull e Valid em cozinha

--- src/main/java/br/com/eduardo/algafood/domain/model/Restaurante.java
+++ src/main/java/br/com/eduardo/algafood/domain/model/Restaurante.java
@@ -16,8 +16,10 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -48,7 +50,8 @@ public class Restaurante {
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 	
-	
+	@Valid
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
