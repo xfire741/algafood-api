5d77dc2b726d6751f3dc81b7797161d0abbd3bad
xfire741 <eduardovictor28@gmail.com>
2023-05-18 11:24:54 -0300

    Selecionando os endpoints da API para gerar a documentação

--- src/main/java/br/com/eduardo/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/br/com/eduardo/algafood/core/openapi/SpringFoxConfig.java
@@ -3,6 +3,7 @@ package br.com.eduardo.algafood.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -14,7 +15,8 @@ public class SpringFoxConfig {
 	public Docket apiDocket() {
 		return new Docket(DocumentationType.OAS_30)
 				.select()
-				.apis(RequestHandlerSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("br.com.eduardo.algafood.api"))
+				.paths(PathSelectors.any())
 				.build();
 	}
 
