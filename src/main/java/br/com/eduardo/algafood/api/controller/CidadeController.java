56ce162089b900ed907d04abe3947beca965f34a
xfire741 <eduardovictor28@gmail.com>
2023-05-19 10:35:18 -0300

    Desacoplando anotações do Swagger dos controladores

--- src/main/java/br/com/eduardo/algafood/api/controller/CidadeController.java
+++ src/main/java/br/com/eduardo/algafood/api/controller/CidadeController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.eduardo.algafood.api.assembler.CidadeInputDisassembler;
 import br.com.eduardo.algafood.api.assembler.CidadeModelAssembler;
-import br.com.eduardo.algafood.api.exceptionhandler.Problem;
+import br.com.eduardo.algafood.api.controller.openapi.CidadeControllerOpenApi;
 import br.com.eduardo.algafood.api.model.CidadeDTO;
 import br.com.eduardo.algafood.api.model.input.CidadeInputDTO;
 import br.com.eduardo.algafood.domain.exception.EstadoNaoEncontradaException;
@@ -26,16 +26,11 @@ import br.com.eduardo.algafood.domain.exception.NegocioException;
 import br.com.eduardo.algafood.domain.model.Cidade;
 import br.com.eduardo.algafood.domain.repository.CidadeRepository;
 import br.com.eduardo.algafood.domain.service.CadastroCidadeService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 
-@Api(tags = "Cidades")
 @RestController
 @RequestMapping("/cidades")
-public class CidadeController {
+public class CidadeController implements CidadeControllerOpenApi {
 
 	@Autowired
 	private CidadeInputDisassembler cidadeInputDisassembler;
@@ -49,26 +44,16 @@ public class CidadeController {
 	@Autowired
 	private CadastroCidadeService cadastroCidade;
 
-	@ApiOperation("Lista as cidades")
 	@GetMapping
 	public List<CidadeDTO> listar() {
 		return cidadeModelAssembler.toCollectionDTO(cidadeRepository.findAll());
 	}
 
-	@ApiOperation("Busca uma cidade por ID")
-	@ApiResponses({
-		@ApiResponse(code = 400, message = "ID da cidade inválido", response = Problem.class),
-		@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
-	})
 	@GetMapping("/{id}")
 	public CidadeDTO buscar(@ApiParam(value = "ID de uma cidade") @PathVariable Long id) {
 		return cidadeModelAssembler.toDTO(cadastroCidade.buscarOuFalhar(id));
 	}
 
-	@ApiOperation("Cadastra uma cidade")
-	@ApiResponses({
-		@ApiResponse(code = 201, message = "Cidade cadastrada")
-	})
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public CidadeDTO adicionar(@ApiParam(name = "corpo", value = "Representação de uma nova cidade")
@@ -81,22 +66,12 @@ public class CidadeController {
 		}
 	}
 
-	@ApiOperation("Exclui uma cidade por ID")
-	@ApiResponses({
-		@ApiResponse(code = 204, message = "Cidade excluída"),
-		@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
-	})
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping("/{id}")
 	public void remover(@ApiParam(value = "ID de uma cidade") @PathVariable Long id) {
 		cadastroCidade.excluir(id);
 	}
 
-	@ApiOperation("Atualiza uma cidade por ID")
-	@ApiResponses({
-		@ApiResponse(code = 200, message = "Cidade atualizada"),
-		@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
-	})
 	@PutMapping("/{id}")
 	public CidadeDTO atualizar(@ApiParam(value = "ID de uma cidade") @PathVariable Long id,
 			@ApiParam(name = "corpo", value = "Representação de uma cidade com os novos dados") 
---
+++ src/main/java/br/com/eduardo/algafood/api/controller/openapi/CidadeControllerOpenApi.java
@@ -0,0 +1,52 @@
+package br.com.eduardo.algafood.api.controller.openapi;
+
+import java.util.List;
+
+import br.com.eduardo.algafood.api.exceptionhandler.Problem;
+import br.com.eduardo.algafood.api.model.CidadeDTO;
+import br.com.eduardo.algafood.api.model.input.CidadeInputDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(tags = "Cidades")
+public interface CidadeControllerOpenApi {
+
+	@ApiOperation("Lista as cidades")
+	public List<CidadeDTO> listar();
+
+	@ApiOperation("Busca uma cidade por ID")
+	@ApiResponses({
+		@ApiResponse(code = 400, message = "ID da cidade inválido", response = Problem.class),
+		@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
+	})
+	public CidadeDTO buscar(@ApiParam(value = "ID de uma cidade") Long id);
+		
+
+	@ApiOperation("Cadastra uma cidade")
+	@ApiResponses({
+		@ApiResponse(code = 201, message = "Cidade cadastrada")
+	})
+	public CidadeDTO adicionar(@ApiParam(name = "corpo", value = "Representação de uma nova cidade")
+			CidadeInputDTO cidadeInputDTO);
+		
+
+	@ApiOperation("Exclui uma cidade por ID")
+	@ApiResponses({
+		@ApiResponse(code = 204, message = "Cidade excluída"),
+		@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
+	})
+	public void remover(@ApiParam(value = "ID de uma cidade") Long id);
+
+	@ApiOperation("Atualiza uma cidade por ID")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Cidade atualizada"),
+		@ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
+	})
+	public CidadeDTO atualizar(@ApiParam(value = "ID de uma cidade") Long id,
+			@ApiParam(name = "corpo", value = "Representação de uma cidade com os novos dados") 
+			CidadeInputDTO cidadeInputDTO);
+	
+}
