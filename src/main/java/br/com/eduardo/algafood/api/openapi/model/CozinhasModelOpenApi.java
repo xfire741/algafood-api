040e3ab42cf367ad5245e17094f88222fa6183e5
xfire741 <eduardovictor28@gmail.com>
2023-08-18 18:00:00 -0300

    Corrigindo a paginação na documentação

--- src/main/java/br/com/eduardo/algafood/api/openapi/model/CozinhasModelOpenApi.java
+++ src/main/java/br/com/eduardo/algafood/api/openapi/model/CozinhasModelOpenApi.java
@@ -1,9 +1,30 @@
 package br.com.eduardo.algafood.api.openapi.model;
 
+import java.util.List;
+
+import org.springframework.hateoas.Links;
+
 import br.com.eduardo.algafood.api.model.CozinhaDTO;
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @ApiModel("CozinhasModel")
-public class CozinhasModelOpenApi extends PageModelOpenApi<CozinhaDTO> {
+@Setter
+@Getter
+public class CozinhasModelOpenApi {
+
+	private CozinhasEmbeddedModelOpenApi _embedded;
+	private Links _links;
+	private PageModelOpenApi page;
+	
+	@ApiModel("CozinhasEmbeddedModel")
+	@Data
+	public class CozinhasEmbeddedModelOpenApi {
+		
+		private List<CozinhaDTO> cozinhas;
+		
+	}
 	
 }
--- src/main/java/br/com/eduardo/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/br/com/eduardo/algafood/core/openapi/SpringFoxConfig.java
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.Links;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -75,7 +76,7 @@ public class SpringFoxConfig {
 						,InputStream.class)
 	            .directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
 	            .alternateTypeRules(AlternateTypeRules.newRule(
-	                    typeResolver.resolve(Page.class, CozinhaDTO.class),
+	                    typeResolver.resolve(PagedModel.class, CozinhaDTO.class),
 	                    CozinhasModelOpenApi.class))
 	            .alternateTypeRules(AlternateTypeRules.newRule(
 	                    typeResolver.resolve(Page.class, PedidoResumoDTO.class),
