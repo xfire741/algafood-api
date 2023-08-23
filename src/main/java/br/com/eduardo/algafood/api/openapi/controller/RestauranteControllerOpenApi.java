d197b74d6901e99d4dd468ba2c3e53eb039982f6
xfire741 <eduardovictor28@gmail.com>
2023-08-23 15:15:07 -0300

    Removendo modelo de representação inutilizado da documentação

--- src/main/java/br/com/eduardo/algafood/api/openapi/controller/RestauranteControllerOpenApi.java
+++ src/main/java/br/com/eduardo/algafood/api/openapi/controller/RestauranteControllerOpenApi.java
@@ -17,6 +17,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Api(tags = "Restaurantes")
 public interface RestauranteControllerOpenApi {
@@ -28,6 +29,7 @@ public interface RestauranteControllerOpenApi {
 	})
 	CollectionModel<RestauranteBasicoModel> listar();
 	
+	@ApiIgnore
 	@ApiOperation(value = "Lista restaurantes", hidden = true)
 	CollectionModel<RestauranteApenasNomeModel> listarApenasNome();
 	
