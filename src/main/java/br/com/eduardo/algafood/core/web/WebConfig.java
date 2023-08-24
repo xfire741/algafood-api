145ec43138b5901687a56280ce84e71dba3adf86
xfire741 <eduardovictor28@gmail.com>
2023-08-24 11:16:00 -0300

    Definindo a versão padrão da API quando o Media Type não é informado

--- src/main/java/br/com/eduardo/algafood/core/web/WebConfig.java
+++ src/main/java/br/com/eduardo/algafood/core/web/WebConfig.java
@@ -5,6 +5,7 @@ import javax.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,6 +16,11 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 		.allowedMethods("*");
+	}
+	
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.defaultContentType(AlgaMediaTypes.V2_APPLICATION_JSON);
 	}
 	
 	@Bean
