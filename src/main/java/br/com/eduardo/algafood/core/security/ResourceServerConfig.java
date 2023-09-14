543df9da6a2abeacc387784eb474fa774a94e303
xfire741 <eduardovictor28@gmail.com>
2023-09-14 11:57:07 -0300

    Configurando o Resource Server com a nova stack do Spring Security

---
+++ src/main/java/br/com/eduardo/algafood/core/security/ResourceServerConfig.java
@@ -0,0 +1,31 @@
+package br.com.eduardo.algafood.core.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SuppressWarnings("deprecation")
+@EnableWebSecurity
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+					.anyRequest().authenticated()
+				.and()
+					.oauth2ResourceServer().opaqueToken();
+					
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
