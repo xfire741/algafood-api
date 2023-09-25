19a4b15dbe468ec12bd7ef75dc2a7c2081d0b6b7
xfire741 <eduardovictor28@gmail.com>
2023-09-25 16:40:15 -0300

    Configurando a validação de JWT no Resource Server com a chave pública

--- src/main/java/br/com/eduardo/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/br/com/eduardo/algafood/core/security/ResourceServerConfig.java
@@ -1,15 +1,8 @@
 package br.com.eduardo.algafood.core.security;
 
-import javax.crypto.spec.SecretKeySpec;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @SuppressWarnings("deprecation")
 @EnableWebSecurity
@@ -26,20 +19,6 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 					.cors().and()
 					.oauth2ResourceServer().jwt();
 					
-	}
-	
-	@Bean
-	public JwtDecoder jwtDecoder() {
-		
-		var secretKey =  new SecretKeySpec("Kp7RbqoXw6e4pL3A9zTtNfYx2vJyD5WQ".getBytes(), "HmacSHA256");
-		
-		return NimbusJwtDecoder.withSecretKey(secretKey).build();
-		
-	}
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 	
 }
