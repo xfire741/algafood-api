50f6cb95170eda171d3a976d2fe091e1a4c378d4
xfire741 <eduardovictor28@gmail.com>
2023-09-27 16:35:41 -0300

    Alterando banco de dados Tabela usuario Coluna senha

---
+++ src/main/resources/db/migration/V021__alterando_coluna_senha_tabela_usuario.sql
@@ -0,0 +1,2 @@
+update usuario set senha = "$2a$12$YBKGf9nBIHx5ITugw6sxvOHT9/jdryi4000BfjY2ZmTQtrCbzMmTO" 
+where id = 1;
--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -83,13 +83,13 @@ insert into grupo (id, nome) values (1, 'Gerente'), (2, 'Vendedor'), (3, 'Secret
 insert into grupo_permissao (grupo_id, permissao_id) values (1, 1), (1, 2), (2, 1), (2, 2), (3, 1); 
 
 insert into usuario (id, nome, email, senha, data_cadastro) values
-(1, 'João da Silva', 'joao.ger@algafood.com', '123', utc_timestamp),
-(2, 'Maria Joaquina', 'maria.vnd@algafood.com', '123', utc_timestamp),
-(3, 'José Souza', 'jose.aux@algafood.com', '123', utc_timestamp),
-(4, 'Sebastião Martins', 'sebastiao.cad@algafood.com', '123', utc_timestamp),
-(5, 'Manoel Lima', 'manoel.loja@gmail.com', '123', utc_timestamp),
-(6, 'Débora Mendonça', 'eduardovictor28@gmail.com', '123', utc_timestamp),
-(7, 'Carlos Lima', 'email.teste.eduardovictor28+carlos@gmail.com', '123', utc_timestamp);
+(1, 'João da Silva', 'joao.ger@algafood.com', '$2a$12$YBKGf9nBIHx5ITugw6sxvOHT9/jdryi4000BfjY2ZmTQtrCbzMmTO', utc_timestamp),
+(2, 'Maria Joaquina', 'maria.vnd@algafood.com', '$2a$12$YBKGf9nBIHx5ITugw6sxvOHT9/jdryi4000BfjY2ZmTQtrCbzMmTO', utc_timestamp),
+(3, 'José Souza', 'jose.aux@algafood.com', '$2a$12$YBKGf9nBIHx5ITugw6sxvOHT9/jdryi4000BfjY2ZmTQtrCbzMmTO', utc_timestamp),
+(4, 'Sebastião Martins', 'sebastiao.cad@algafood.com', '$2a$12$YBKGf9nBIHx5ITugw6sxvOHT9/jdryi4000BfjY2ZmTQtrCbzMmTO', utc_timestamp),
+(5, 'Manoel Lima', 'manoel.loja@gmail.com', '$2a$12$YBKGf9nBIHx5ITugw6sxvOHT9/jdryi4000BfjY2ZmTQtrCbzMmTO', utc_timestamp),
+(6, 'Débora Mendonça', 'eduardovictor28@gmail.com', '$2a$12$YBKGf9nBIHx5ITugw6sxvOHT9/jdryi4000BfjY2ZmTQtrCbzMmTO', utc_timestamp),
+(7, 'Carlos Lima', 'email.teste.eduardovictor28+carlos@gmail.com', '$2a$12$YBKGf9nBIHx5ITugw6sxvOHT9/jdryi4000BfjY2ZmTQtrCbzMmTO', utc_timestamp);
 
 insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2);
 
